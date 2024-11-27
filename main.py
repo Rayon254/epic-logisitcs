e49f873368d7550e5da81692e3eb4a16ff018ca1
Rayon254 <150139046+Rayon254@users.noreply.github.com>
2024-11-27 22:20:10 +0300

    Your commit message here

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
-app.secret_key = 'ksfgj'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
+    firstname = lastname = emailaddress = phonenumber = None
+
     if request.method == 'POST':
-        print("Form submitted")
+       
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
         emailaddress = request.form.get('emailaddress')
         phonenumber = request.form.get('phonenumber')
-        
-        # Print the received form data to the console
-        print(f"Received data: {firstname}, {lastname}, {emailaddress}, {phonenumber}")
-        
-        return render_template('index.html', firstname=firstname, lastname=lastname,
-                               emailaddress=emailaddress, phonenumber=phonenumber)
-    
-    return render_template('index.html')
+
+  
+
+    return render_template(
+        'index.html',
+        firstname=firstname,
+        lastname=lastname,
+        emailaddress=emailaddress,
+        phonenumber=phonenumber
+    )
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=5000)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+
+
+
 
